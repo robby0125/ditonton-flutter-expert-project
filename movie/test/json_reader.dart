fcf03b5693f3ddca0dcd4222662da530e1619640
Robby Alamsyah <robbyalamsyah025@gmail.com>
2022-09-10 20:21:02 +0700

    change read json path

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -2,11 +2,14 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
+
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  if (dir.endsWith('/ditonton')) {
+
+  if (!dir.endsWith('/movie')) {
     dir += '/movie';
   }
+
   return File('$dir/test/$name').readAsStringSync();
 }
